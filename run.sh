ac8587cedb685412215fa41ea3e36bcbd6fabff2
Nithin N P <nithin.n.p@pwc.com>
2023-05-09 19:27:21 +0530

    changed bash script

--- run.sh
+++ run.sh
@@ -1,38 +1,12 @@
-sudo apt-get -y update
-
-sudo apt-get install -y default-jdk
-
-sudo apt install -y maven
-
-sudo apt-get install unzip
-
-
-wget https://chromedriver.storage.googleapis.com/113.0.5672.63/chromedriver_linux64.zip
-
-unzip chromedriver_linux64.zip
-
-sudo mv chromedriver /usr/local/bin/
-
-sudo apt-get install -y libappindicator1 fonts-liberation
-
-sudo apt-get install -f
-
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
-
-sudo dpkg -i google-chrome*.deb
-
-sudo apt-get install -y libgbm-dev
-
-sudo apt --fix-broken -y install
-
-google-chrome-stable -version
-
 cd testmvnjava
+
+echo "------------------performing maven operations----------------------"
 
 mvn clean install
 
 mvn exec:java -Dexec.mainClass="org.example.App"
+
+echo "------------------Done excecution----------------------"
 
  
 
